746875eaf5c94a35ea85f7b86814c6c33164fb46
Arsalan831 <arsalanbashir831@gmail.com>
2024-12-28 15:57:27 +0500

    admin dashboard registeration

--- attendancelisting/admin.py
+++ attendancelisting/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import Attendance
+from unfold.admin import ModelAdmin
 
 @admin.register(Attendance)
-class AttendanceAdmin(admin.ModelAdmin):
+class AttendanceAdmin(ModelAdmin):
     list_display = ('user', 'creation_date', 'reporting_time', 'checkout_time', 'working_hours', 'overtime_hours')
     search_fields = ('user__username', 'creation_date')
     list_filter = ('creation_date',)
--- leaveapplication/admin.py
+++ leaveapplication/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from .models import LeaveApplication
+from unfold.admin import ModelAdmin
 
-# Register your models here.
+@admin.register(LeaveApplication)
+class LeaveApplicationAdmin(ModelAdmin):
+    # Fields to display in the admin list view
+    list_display = (
+        'user', 
+        'creation_date', 
+        'start_date', 
+        'end_date', 
+        'reason', 
+        'is_approved'
+    )
+    
+    # Fields to filter by in the admin interface
+    list_filter = (
+        'is_approved', 
+        'creation_date', 
+        'start_date', 
+        'end_date'
+    )
+    
+    # Fields to search in the admin interface
+    search_fields = (
+        'user__username', 
+        'reason'
+    )
+    
+    # Fields to show in the detail view of the admin interface
+   
+    # Fields to display as read-only
+    readonly_fields = (
+        'creation_date',
+    )
+    
+    # Order of fields in the admin list view
+    ordering = ('-creation_date',)
